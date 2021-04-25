01634e21aa98109bf37f36b6b9add3f7345fd73d
zahwanugrah <39833642+zahwanugrah@users.noreply.github.com>
2021-04-25 14:23:59 +0700

    Update mn.sh

--- mn.sh
+++ mn.sh
@@ -46,7 +46,7 @@ echo "--------------- Welcome To Premium Script Menu ---------------"
 	echo -e ""
   echo -e "------------------[ V2RAY/VMESS OPTION  ]---------------"
   echo -e "${color1}11${color3}.Create V2ray/Vmess Account (${color2}add-ws${color3})"
-	echo -e "${color1}12${color3}.Delete V2ray/Vmess Account (${color2}del-ws${color3})"
+  echo -e "${color1}12${color3}.Delete V2ray/Vmess Account (${color2}del-ws${color3})"
   echo -e "${color1}13${color3}.Extend V2ray/Vmess Account (${color2}renew-ws${color3})"
   echo -e "${color1}14${color3}.Check User Login (${color2}cek-ws${color3})"
   echo -e ""
@@ -87,126 +87,159 @@ echo "--------------- Welcome To Premium Script Menu ---------------"
   echo -e "${color1}38${color3}.Extend L2TP Account (${color2}renew-l2tp${color3})"
   echo -e " "
   echo -e "------------------[  PPTP  OPTION ]---------------"
-  echo -e "${color1}38${color3}.Create PPTP Account (${color2}add-pptp${color3})"
-  echo -e "${color1}39${color3}.Delete PPTP Account (${color2}del-pptp${color3})"
-  echo -e "${color1}40${color3}.Extend PPTP Account (${color2}renew-pptp${color3})"
-  echo -e "${color1}41${color3}.Check User Login (${color2}cek-pptp${color3})"
+  echo -e "${color1}39${color3}.Create PPTP Account (${color2}add-pptp${color3})"
+  echo -e "${color1}40${color3}.Delete PPTP Account (${color2}del-pptp${color3})"
+  echo -e "${color1}41${color3}.Extend PPTP Account (${color2}renew-pptp${color3})"
+  echo -e "${color1}42${color3}.Check User Login (${color2}cek-pptp${color3})"
   echo -e " "
   echo -e "------------------[  SSTP  OPTION ]---------------"
-  echo -e "${color1}42${color3}.Create SSTP Account (${color2}add-sstp${color3})"
-  echo -e "${color1}43${color3}.Delete SSTP Account (${color2}del-sstp${color3})"
-  echo -e "${color1}44${color3}.Extend SSTP Account (${color2}renew-sstp${color3})"
-  echo -e "${color1}45${color3}.Check User Login (${color2}cek-sstp${color3})"
+  echo -e "${color1}43${color3}.Create SSTP Account (${color2}add-sstp${color3})"
+  echo -e "${color1}44${color3}.Delete SSTP Account (${color2}del-sstp${color3})"
+  echo -e "${color1}45${color3}.Extend SSTP Account (${color2}renew-sstp${color3})"
+  echo -e "${color1}46${color3}.Check User Login (${color2}cek-sstp${color3})"
   echo -e " "
-	echo -e "-----------------[ SYSTEM OPTION ]-----------------"
-	echo -e "${color1}46${color3}.Speedtest Server (${color2}speedtest${color3})"
-	echo -e "${color1}24${color3}.Add Or Change Subdomain Host For VPS (${color2}add-host${color3})"
-	echo -e "${color1}25${color3}.Change Port Of Some Service (${color2}change-port${color3})"
-	echo -e "${color1}26${color3}.Restart Stunnel (${color2}service stunnel restart${color3})"
-	if [[ "$OS" = 'debian' ]]; then 
-	echo -e "${color1}27${color3}.Restart OpenSSH (${color2}service ssh restart${color3})"
-	echo -e "${color1}28${color3}.Restart Dropbear (${color2}service dropbear restart${color3})"
-	echo -e "${color1}29${color3}.Restart OpenVPN (${color2}service openvpn restart${color3})"
-	echo -e "${color1}30${color3}.Restart Squid Proxy (${color2}service squid3 restart${color3})"
-else
-	echo -e "${color1}27${color3}.Restart OpenSSH (${color2}service sshd restart${color3})"
-	echo -e "${color1}28${color3}.Restart Dropbear (${color2}service dropbear restart${color3})"
-	echo -e "${color1}29${color3}.Restart OpenVPN (${color2}service openvpn restart${color3})"
-	echo -e "${color1}30${color3}.Restart Squid Proxy (${color2}service squid restart${color3})"
-fi
-echo -e "${color1}31${color3}.Edit Server Port (${color2}edit-port${color3})"
-echo -e "${color1}32${color3}.Set Auto Reboot (${color2}auto-reboot${color3})"
-echo -e "${color1}33${color3}.Reboot VPS(${color2}reboot${color3})"
-echo -e "${color1}34${color3}.Change Server Password (${color2}passwd${color3})"
-echo -e " "
-echo -e "-----=[ Others ]=-----"
-echo -e "${color1}35${color3}.View Installation Log (${color2}log-install${color3})"
-echo -e "${color1}36${color3}.VPS Diagnostics (${color2}diagnosa${color3})"
-echo -e "${color1}37${color3}.Exit Menu (${color2}exit${color3})"
-echo "------------------------Script by FordSenpai-----------------------------"
+  echo -e "-----------------[ SYSTEM OPTION ]-----------------"
+  echo -e "${color1}47${color3}.Speedtest Server (${color2}speedtest${color3})"
+  echo -e "${color1}48${color3}.Add Or Change Subdomain Host For VPS (${color2}add-host${color3})"
+  echo -e "${color1}49${color3}.Change Port Of Some Service (${color2}change-port${color3})"
+  echo -e "${color1}50${color3}.Autobackup Data VPS (${color2}autobackup${color3})"
+  echo -e "${color1}51${color3}.Backup Data VPS (${color2}backup${color3})"
+  echo -e "${color1}52${color3}.Restore Data VPS (${color2}restore${color3})"
+  echo -e "${color1}53${color3}.Webmin Menu (${color2}wbmn${color3})"
+  echo -e "${color1}54${color3}.Update To Latest Kernel (${color2}kernel-updt${color3})"
+  echo -e "${color1}55${color3}.Limit Bandwith Speed Server (${color2}limit-speed ${color3})"
+  echo -e "${color1}56${color3}.Check Usage of VPS Ram (${color2}ram ${color3})"
+  echo -e "${color1}57${color3}.Reboot VPS (${color2}service squid reboot${color3})"
+  echo -e "${color1}58${color3}.Update To Latest Script Version (${color2}update${color3})"
+  echo -e "${color1}59${color3}.Reboot VPS(${color2}reboot${color3})"
+  echo -e "${color1}60${color3}.Change Server Password (${color2}passwd${color3})"
+  echo -e "${color1}61${color3}.Renew Certificate V2ray (${color2}cert2vray${color3})"
+  echo -e "${color1}62${color3}.Exit Menu (${color2}exit${color3})"
+  echo "------------------------Script Mod By Zhang-Zi-----------------------------"
 read -p "Choose an option from (1-37): " x
 if test $x -eq 1; then
-user-add
+usernew
 elif test $x -eq 2; then
-user-generate
-elif test $x -eq 3; then
 trial
+elif test $x -eq 3; then
+renew
 elif test $x -eq 4; then
-user-aktif
+deluser
 elif test $x -eq 5; then
-user-password
+cek
 elif test $x -eq 6; then
-read -p "Select Maximum Login (1-2): " MULTILOGIN
-user-ban $MULTILOGIN
+member
 elif test $x -eq 7; then
-user-unban
+delete
 elif test $x -eq 8; then
-user-lock
-elif test $x -eq 9; then
-user-unlock
-elif test $x -eq 10; then
-user-delete
-elif test $x -eq 11; then
-user-detail
-elif test $x -eq 12; then
-user-list
-elif test $x -eq 13; then
-user-login
-elif test $x -eq 14; then
-user-log
-elif test $x -eq 15; then
-read -p "Select Maximum Login (1-2): " MULTILOGIN
-user-limit $MULTILOGIN
-elif test $x -eq 16; then
 autokill
+elif test $x -eq 9; then
+ceklim
+elif test $x -eq 10; then
+restart
+elif test $x -eq 11; then
+add-ws
+elif test $x -eq 12; then
+del-ws
+elif test $x -eq 13; then
+renew-ws
+elif test $x -eq 14; then
+cek-ws
+elif test $x -eq 15; then
+add-vless
+elif test $x -eq 16; then
+del-vless
 elif test $x -eq 17; then
-infouser
+renew-vless
 elif test $x -eq 18; then
-expireduser
+cek-ws
 elif test $x -eq 19; then
-user-delete-expired
+add-tr
 elif test $x -eq 20; then
-clear
-echo "This script runs automatically every 12 hours"
-echo "You don't need to run it manually"
-echo "If you still want to run this script, type user-expire"
+del-tr
 elif test $x -eq 21; then
-log-limit
+renew-tr
 elif test $x -eq 22; then
-log-ban
+cek-tr
 elif test $x -eq 23; then
-speedtest --share
+add-ss
 elif test $x -eq 24; then
-bench-network
+del-ss
 elif test $x -eq 25; then
-ram
+renew-ss
 elif test $x -eq 26; then
-service stunnel4 restart
+cek-ss
 elif test $x -eq 27; then
-	if [[ "$OS" = 'debian' ]]; then 
-		service ssh restart 
-	else 
-		service sshd restart 
-	fi
+add-ssr
 elif test $x -eq 28; then
-service dropbear restart
+del-ssr
 elif test $x -eq 29; then
-service openvpn restart
+renew-ssr
 elif test $x -eq 30; then
-service squid restart
+ssr
 elif test $x -eq 31; then
-edit-port
+add-wg
 elif test $x -eq 32; then
-auto-reboot
+del-wg
 elif test $x -eq 33; then
-reboot
+cek-wg
 elif test $x -eq 34; then
-passwd
+renew-wg
 elif test $x -eq 35; then
-log-install
+wg show
 elif test $x -eq 36; then
-diagnosa
+add-l2tp
 elif test $x -eq 37; then
+del-l2tp
+elif test $x -eq 38; then
+renew-l2tp
+elif test $x -eq 39; then
+add-pptp
+elif test $x -eq 40; then
+del-pptp
+elif test $x -eq 41; then
+renew-pptp
+elif test $x -eq 42; then
+cek-pptp
+elif test $x -eq 43; then
+add-sstp
+elif test $x -eq 44; then
+del-sstp
+elif test $x -eq 45; then
+renew-sstp
+elif test $x -eq 46; then
+cek-sstp
+elif test $x -eq 47; then
+speedtest
+elif test $x -eq 48; then
+add-host
+elif test $x -eq 49; then
+change-port
+elif test $x -eq 50; then
+autobackup
+elif test $x -eq 51; then
+backup
+elif test $x -eq 52; then
+restore
+elif test $x -eq 53; then
+wbmn
+elif test $x -eq 54; then
+kernel-updt
+elif test $x -eq 55; then
+limit-speed
+elif test $x -eq 56; then
+ram
+elif test $x -eq 57; then
+reboot
+elif test $x -eq 58; then
+update
+elif test $x -eq 59; then
+reboot
+elif test $x -eq 60; then
+passwd
+elif test $x -eq 61; then
+cert2vray
+elif test $x -eq 62; then
 echo " "
 echo "Goodbye Menu!"
 echo " "
