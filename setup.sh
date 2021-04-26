3f115c815b956a67dca391186cbb22f575c2fcf6
zahwanugrah <39833642+zahwanugrah@users.noreply.github.com>
2021-04-26 19:13:26 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -19,7 +19,7 @@ mkdir /var/lib/premium-script;
 echo "IP=" >> /var/lib/premium-script/ipvps.conf
 wget https://raw.githubusercontent.com/zahwanugrah/auto/main/cf.sh && chmod +x cf.sh && ./cf.sh
 #install ssh ovpn
-wget https://raw.githubusercontent.com/zahwanugrah/auto/main/ssh-vpn.sh && chmod +x ssh-vpn.sh && screen -S ssh-vpn ./ssh-vpn.sh
+wget https://raw.githubusercontent.com/zahwanugrah/AutoScriptSSH/main/ssh-vpn.sh && chmod +x ssh-vpn.sh && screen -S ssh-vpn ./ssh-vpn.sh
 wget https://raw.githubusercontent.com/zahwanugrah/auto/main/sstp.sh && chmod +x sstp.sh && screen -S sstp ./sstp.sh
 #install ssr
 wget https://raw.githubusercontent.com/zahwanugrah/auto/main/ssr.sh && chmod +x ssr.sh && screen -S ssr ./ssr.sh
