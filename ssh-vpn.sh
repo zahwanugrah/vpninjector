76955df4cec2d18dfbebcd608a7801bd32d22aac
zahwanugrah <39833642+zahwanugrah@users.noreply.github.com>
2021-04-26 10:21:41 +0700

    Update ssh-vpn.sh

--- ssh-vpn.sh
+++ ssh-vpn.sh
@@ -21,7 +21,7 @@ commonname=www.vpninjector.com
 email=admin@vpninjector.com
 
 # simple password minimal
-wget -O /etc/pam.d/common-password "https://raw.githubusercontent.com/zahwanugrah/main/password"
+wget -O /etc/pam.d/common-password "https://raw.githubusercontent.com/zahwanugrah/auto/main/password"
 chmod +x /etc/pam.d/common-password
 
 # go to root
@@ -88,14 +88,14 @@ apt -y install nginx
 cd
 rm /etc/nginx/sites-enabled/default
 rm /etc/nginx/sites-available/default
-wget -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/zahwanugrah/main/nginx.conf"
+wget -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/zahwanugrah/auto/main/nginx.conf"
 mkdir -p /home/vps/public_html
-wget -O /etc/nginx/conf.d/vps.conf "https://raw.githubusercontent.com/zahwanugrah/main/vps.conf"
+wget -O /etc/nginx/conf.d/vps.conf "https://raw.githubusercontent.com/zahwanugrah/auto/main/vps.conf"
 /etc/init.d/nginx restart
 
 # install badvpn
 cd
-wget -O /usr/bin/badvpn-udpgw "https://raw.githubusercontent.com/zahwanugrah/main/badvpn-udpgw64"
+wget -O /usr/bin/badvpn-udpgw "https://raw.githubusercontent.com/zahwanugrah/auto/main/badvpn-udpgw64"
 chmod +x /usr/bin/badvpn-udpgw
 sed -i '$ i\screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7100 --max-clients 500' /etc/rc.local
 sed -i '$ i\screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7200 --max-clients 500' /etc/rc.local
@@ -125,7 +125,7 @@ echo "/usr/sbin/nologin" >> /etc/shells
 # install squid
 cd
 apt -y install squid3
-wget -O /etc/squid/squid.conf "https://raw.githubusercontent.com/zahwanugrah/main/squid3.conf"
+wget -O /etc/squid/squid.conf "https://raw.githubusercontent.com/zahwanugrah/auto/main/squid3.conf"
 sed -i $MYIP2 /etc/squid/squid.conf
 
 # setting vnstat
@@ -171,7 +171,7 @@ sed -i 's/ENABLED=0/ENABLED=1/g' /etc/default/stunnel4
 /etc/init.d/stunnel4 restart
 
 #OpenVPN
-wget https://raw.githubusercontent.com/zahwanugrah/main/vpn.sh &&  chmod +x vpn.sh && ./vpn.sh
+wget https://raw.githubusercontent.com/zahwanugrah/auto/main/vpn.sh &&  chmod +x vpn.sh && ./vpn.sh
 
 # install fail2ban
 apt -y install fail2ban
@@ -226,36 +226,35 @@ netfilter-persistent reload
 
 # download script
 cd /usr/bin
-wget -O add-host "https://raw.githubusercontent.com/zahwanugrah/main/add-host.sh"
-wget -O about "https://raw.githubusercontent.com/zahwanugrah/main/about.sh"
-wget -O menu "https://raw.githubusercontent.com/zahwanugrah/main/menu.sh"
-wget -O usernew "https://raw.githubusercontent.com/zahwanugrah/main/usernew.sh"
-wget -O trial "https://raw.githubusercontent.com/zahwanugrah/main/trial.sh"
-wget -O hapus "https://raw.githubusercontent.com/zahwanugrah/main/hapus.sh"
-wget -O member "https://raw.githubusercontent.com/zahwanugrah/main/member.sh"
-wget -O delete "https://raw.githubusercontent.com/zahwanugrah/main/delete.sh"
-wget -O cek "https://raw.githubusercontent.com/zahwanugrah/main/cek.sh"
-wget -O restart "https://raw.githubusercontent.com/zahwanugrah/main/restart.sh"
-wget -O speedtest "https://raw.githubusercontent.com/zahwanugrah/main/speedtest_cli.py"
-wget -O info "https://raw.githubusercontent.com/zahwanugrah/main/info.sh"
-wget -O ram "https://raw.githubusercontent.com/zahwanugrah/main/ram.sh"
-wget -O renew "https://raw.githubusercontent.com/zahwanugrah/main/renew.sh"
-wget -O autokill "https://raw.githubusercontent.com/zahwanugrah/main/autokill.sh"
-wget -O ceklim "https://raw.githubusercontent.com/zahwanugrah/main/ceklim.sh"
-wget -O tendang "https://raw.githubusercontent.com/zahwanugrah/main/tendang.sh"
-wget -O clear-log "https://raw.githubusercontent.com/zahwanugrah/main/clear-log.sh"
-wget -O change-port "https://raw.githubusercontent.com/zahwanugrah/main/change.sh"
-wget -O port-ovpn "https://raw.githubusercontent.com/zahwanugrah/main/port-ovpn.sh"
-wget -O port-ssl "https://raw.githubusercontent.com/zahwanugrah/main/port-ssl.sh"
-wget -O port-wg "https://raw.githubusercontent.com/zahwanugrah/main/port-wg.sh"
-wget -O port-tr "https://raw.githubusercontent.com/zahwanugrah/main/port-tr.sh"
-wget -O port-sstp "https://raw.githubusercontent.com/zahwanugrah/main/port-sstp.sh"
-wget -O port-squid "https://raw.githubusercontent.com/zahwanugrah/main/port-squid.sh"
-wget -O port-ws "https://raw.githubusercontent.com/zahwanugrah/main/port-ws.sh"
-wget -O port-vless "https://raw.githubusercontent.com/zahwanugrah/main/port-vless.sh"
-wget -O wbmn "https://raw.githubusercontent.com/zahwanugrah/main/webmin.sh"
-wget -O xp "https://raw.githubusercontent.com/zahwanugrah/main/xp.sh"
-wget -O kernel-updt "https://raw.githubusercontent.com/zahwanugrah/main/kernel-update.sh"
+wget -O add-host "https://raw.githubusercontent.com/zahwanugrah/auto/main/add-host.sh"
+wget -O menu "https://raw.githubusercontent.com/zahwanugrah/auto/main/menu.sh"
+wget -O usernew "https://raw.githubusercontent.com/zahwanugrah/auto/main/usernew.sh"
+wget -O trial "https://raw.githubusercontent.com/zahwanugrah/auto/main/trial.sh"
+wget -O hapus "https://raw.githubusercontent.com/zahwanugrah/auto/main/hapus.sh"
+wget -O member "https://raw.githubusercontent.com/zahwanugrah/auto/main/member.sh"
+wget -O delete "https://raw.githubusercontent.com/zahwanugrah/auto/main/delete.sh"
+wget -O cek "https://raw.githubusercontent.com/zahwanugrah/auto/main/cek.sh"
+wget -O restart "https://raw.githubusercontent.com/zahwanugrah/auto/main/restart.sh"
+wget -O speedtest "https://raw.githubusercontent.com/zahwanugrah/auto/main/speedtest_cli.py"
+wget -O info "https://raw.githubusercontent.com/zahwanugrah/auto/main/info.sh"
+wget -O ram "https://raw.githubusercontent.com/zahwanugrah/auto/main/ram.sh"
+wget -O renew "https://raw.githubusercontent.com/zahwanugrah/auto/main/renew.sh"
+wget -O autokill "https://raw.githubusercontent.com/zahwanugrah/auto/main/autokill.sh"
+wget -O ceklim "https://raw.githubusercontent.com/zahwanugrah/auto/main/ceklim.sh"
+wget -O tendang "https://raw.githubusercontent.com/zahwanugrah/auto/main/tendang.sh"
+wget -O clear-log "https://raw.githubusercontent.com/zahwanugrah/auto/main/clear-log.sh"
+wget -O change-port "https://raw.githubusercontent.com/zahwanugrah/auto/main/change.sh"
+wget -O port-ovpn "https://raw.githubusercontent.com/zahwanugrah/auto/main/port-ovpn.sh"
+wget -O port-ssl "https://raw.githubusercontent.com/zahwanugrah/auto/main/port-ssl.sh"
+wget -O port-wg "https://raw.githubusercontent.com/zahwanugrah/auto/main/port-wg.sh"
+wget -O port-tr "https://raw.githubusercontent.com/zahwanugrah/auto/main/port-tr.sh"
+wget -O port-sstp "https://raw.githubusercontent.com/zahwanugrah/auto/main/port-sstp.sh"
+wget -O port-squid "https://raw.githubusercontent.com/zahwanugrah/auto/main/port-squid.sh"
+wget -O port-ws "https://raw.githubusercontent.com/zahwanugrah/auto/main/port-ws.sh"
+wget -O port-vless "https://raw.githubusercontent.com/zahwanugrah/auto/main/port-vless.sh"
+wget -O wbmn "https://raw.githubusercontent.com/zahwanugrah/auto/main/webmin.sh"
+wget -O xp "https://raw.githubusercontent.com/zahwanugrah/auto/main/xp.sh"
+wget -O kernel-updt "https://raw.githubusercontent.com/zahwanugrah/auto/main/kernel-update.sh"
 chmod +x add-host
 chmod +x menu
 chmod +x usernew
