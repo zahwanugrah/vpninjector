4f1f84275ad4c7fead3801f7c1bd526bab8cd0f1
zahwanugrah <39833642+zahwanugrah@users.noreply.github.com>
2021-05-19 09:33:34 +0700

    Update add-ws.sh

--- add-ws.sh
+++ add-ws.sh
@@ -4,14 +4,6 @@ green='\e[0;32m'
 NC='\e[0m'
 MYIP=$(wget -qO- icanhazip.com);
 echo "Checking VPS"
-IZIN=$( curl https://raw.githubusercontent.com/zahwanugrah/vpninjector/main/ipvps.conf | grep $MYIP )
-if [ $MYIP = $IZIN ]; then
-echo -e "${green}Permission Accepted...${NC}"
-else
-echo -e "${red}Permission Denied!${NC}";
-echo "Only For Premium Users"
-exit 0
-fi
 clear
 source /var/lib/premium-script/ipvps.conf
 if [[ "$IP" = "" ]]; then
