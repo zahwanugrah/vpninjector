9c3aedc77f5c9a735e090a47bc2362e4cd2f4830
zahwanugrah <39833642+zahwanugrah@users.noreply.github.com>
2021-05-20 16:47:55 +0700

    Update cf.sh

--- cf.sh
+++ cf.sh
@@ -5,11 +5,11 @@ NC='\e[0m'
 MYIP=$(wget -qO- icanhazip.com);
 echo "Checking VPS"
 apt install jq curl -y
-DOMAIN=vpninjector.com
+DOMAIN=myvpnkuy.xyz
 sub=$(</dev/urandom tr -dc a-z0-9 | head -c4)
-SUB_DOMAIN=${sub}.vpninjector.com
-CF_ID=baxiamania@gmail.com
-CF_KEY=e790e6961a311853568fbccd227e043533482
+SUB_DOMAIN=${sub}.myvpnkuy.xyz
+CF_ID=amsalsiregar12@gmail.com
+CF_KEY=d7646d2edd8f564e0da635c6b8b71c5fba0e2
 set -euo pipefail
 IP=$(wget -qO- icanhazip.com);
 echo "Updating DNS for ${SUB_DOMAIN}..."
